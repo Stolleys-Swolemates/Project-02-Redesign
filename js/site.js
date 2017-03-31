b13d6de3c5efd9514a111ce0f18600d02ed2be1e
mflavin <mflavin@hawk.iit.edu>
2017-03-30 19:26:22 -0500

    Adds a class that hides the label when an input is entered.

--- js/site.js
+++ js/site.js
@@ -6,4 +6,19 @@ $(document).ready(function () {
   $("#loginForm").on('submit', function (e) {
     
   });
+  
+  $("#usernameInput").on('focus', function() {
+    $('#user label').addClass('gone');
+    if( $('#passwordInput').val().length === 0) {
+      $('#pass label').removeClass('gone');
+    }
+  });
+  
+  $("#passwordInput").on('focus', function() {
+    $('#pass label').addClass('gone');
+    if( $('#usernameInput').val().length === 0) {
+      $('#user label').removeClass('gone');
+    }
+  });
+ 
 });
