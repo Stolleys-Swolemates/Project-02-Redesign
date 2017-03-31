3faf76e2dd1d54b0e01b6e8e2aeda6c22fcce26a
Ken Ellorando <kenellorando@gmail.com>
2017-03-30 23:34:50 -0500

    Added a couple of comments

--- js/site.js
+++ js/site.js
@@ -6,14 +6,17 @@ $(document).ready(function () {
   $("#loginForm").on('submit', function (e) {
   });
   
+  // Hides username label when on focus
   $("#usernameInput").on('focus', function() {
     $('#user label').addClass('gone');
   });
   
+  // Hides password label when on focus
   $("#passwordInput").on('focus', function() {
     $('#pass label').addClass('gone');
   });
   
+  // Restores labels if input length is zero
   $('html').focusout(function() {
     if( $('#usernameInput').val().length === 0) {
       $('#user label').removeClass('gone');
