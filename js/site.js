c9ae815b663474ee622d2e3ae6206325f420b1d5
Bobby Ton <bobbyt1997@gmail.com>
2017-04-16 23:57:58 -0500

    Removed unnecessary comments

--- js/site.js
+++ js/site.js
@@ -1,6 +1,3 @@
-// Stolley only wants one js file
-// Also, avoid using "onclick" when you guys do html tags
-
 $(document).ready(function () {
   // Frontpage submit
   $("#loginForm").on('submit', function (e) {
